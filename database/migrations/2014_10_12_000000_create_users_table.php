8e6b40ce1981ab9e43c4777883911973fe6f3a93
Tomislav <tvinkovi@mathos.hr>
2022-03-11 10:59:15 +0100

    Initial no db

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->rememberToken();
+
+            $table->string('oib', 11);
+            $table->string('name', 100);
+            $table->string('last_name', 100);
+            $table->string('residence', 100);
+            $table->date('date_of_birth');
+            $table->integer('available_vacation_days', false, true);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
